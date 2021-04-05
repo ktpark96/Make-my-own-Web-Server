5e60a5a3840ff6e584cfcf7a2462ac10c0d498bd
Kitae Park <kitae16@assam.dankook.ac.kr>
2021-04-05 17:52:03 +0900

    04/05 Commit

--- server.c
+++ server.c
@@ -10,29 +10,25 @@
 #include <signal.h>
 #include <fcntl.h>
 
-char buffer[1024];
+#define CONNMAX 1000
 
-void setHttpHeader(char httpHeader[])
-{
-    // File object to return
-    FILE *htmlData = fopen("test.html", "r");
+int client_sock[CONNMAX];
 
-    char line[100];
-    char responseData[8000];
-    while (fgets(line, 100, htmlData) != 0) {
-        strcat(responseData, line);
-    }
-    strcat(httpHeader, responseData);
-}
+char *ROOT;
+
+
+void respond(int n);
 
 int main(int argc, char *argv[])
 {
-        char httpHeader[8000] = "HTTP/1.1 200 OK\r\n\n";
-        int server_sock, client_sock;
+        int server_sock;
         int listenfd;
         ssize_t len;
         pid_t childpid;
-        int ret = 0;
+        int n = 0, ret = 0;
+	
+
+	ROOT = getenv("PWD");
 
 
         if(argc != 2)
@@ -41,7 +37,11 @@ int main(int argc, char *argv[])
                 exit(1);
         }
         int port_num = atoi(argv[1]);
+	
+	for(int i = 0; i < CONNMAX; i++){client_sock[i] = -1;}
 
+	
+	
         // Socket Creation Part
         if((server_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
         {
@@ -71,23 +71,76 @@ int main(int argc, char *argv[])
                 exit(1);
         }
 
-
-
-        setHttpHeader(httpHeader);
-
         // Accept Part
         while(1)
         {
-                if((client_sock = accept(server_sock, NULL, NULL)) < 0)
+                if((client_sock[n] = accept(server_sock, NULL, NULL)) < 0)
                 {
                         perror("accept error");
                         close(server_sock);
                         exit(1);
                 }
-                send(client_sock, httpHeader, sizeof(httpHeader), 0);
-                close(client_sock);
+		else
+		{
+			if(fork() == 0)
+			{
+				respond(n);
+				exit(0);
+			}
+		}
+
+		while(client_sock[n] != -1) {n=(n+1)%CONNMAX;}
         }
 
         return 0;
 
+}
+
+
+void respond(int n)
+{
+	char msg[100000], *reqline[3], data_to_send[1024], path[99999];
+	int rcvd, fd, bytes_read;
+
+	memset((void *)msg, 0, 10000);
+
+	rcvd = recv(client_sock[n], msg, 10000, 0);
+
+	if (rcvd<0) { printf("Receive Error\n"); }
+        else if (rcvd==0) { printf("Client Disconnected\n"); }
+        else    // Receive Success
+        {
+                printf("%s\n", msg);
+                reqline[0] = strtok (msg, " \t\n");
+                
+		if ( strncmp(reqline[0], "GET\0", 4)==0 )
+                {
+                        reqline[1] = strtok (NULL, " \t");
+                        reqline[2] = strtok (NULL, " \t\n");
+                        if ( strncmp( reqline[2], "HTTP/1.0", 8)!=0 && strncmp( reqline[2], "HTTP/1.1", 8)!=0 )
+                        {
+                                write(client_sock[n], "HTTP/1.0 400 Bad Request\n", 25);
+                        }
+                        else
+                        {
+                                if ( strncmp(reqline[1], "/\0", 2)==0 )
+                                        reqline[1] = "/index.html";        //Because if no file is specified, index.html will be opened by default (like it happens in APACHE...
+
+                                strcpy(path, ROOT);
+                                strcpy(&path[strlen(ROOT)], reqline[1]);
+                                printf("file: %s\n\n\n\n", path);
+
+                                if ( (fd=open(path, O_RDONLY))!=-1 )    //FILE FOUND
+                                {
+                                        send(client_sock[n], "HTTP/1.0 200 OK\n\n", 17, 0);
+                                        while ( (bytes_read=read(fd, data_to_send, 1024))>0 )
+						write (client_sock[n], data_to_send, bytes_read);
+                                }
+                                else    write(client_sock[n], "HTTP/1.0 404 Not Found\n", 23); //FILE NOT FOUND
+                        }
+                }
+        }
+	shutdown(client_sock[n], SHUT_RDWR);
+	close(client_sock[n]);
+	client_sock[n] = -1;
 }
